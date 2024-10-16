99e13f92895ecf89ef1ba2ee77104ad10e46ca22
SafaFarah <kangjisook13@gmail.com>
2024-10-16 23:22:55 +0300

    update messageRoute for get message

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import Chat from "../models/chat.js";
 import Message from "../models/message.js";
 import { verify_token } from "../token.js"
@@ -30,14 +30,33 @@ router.post("/send/:id", verify_token, async (req, res) => {
         if(newMessage) {
             chat.messages.push(newMessage._id);
         }
-        await chat.save();
-        await newMessage.save();
-        return res.status(201).json(newMessage);
+        await Promise.all([chat.save(), newMessage.save()]);
+        res.status(201).json(newMessage);
 
     } catch (error) {
         console.error("Error sending message: ", error.message);
-        return res.status(500).json({ error: "Internal server error" });
+        res.status(500).json({ error: "Internal server error" });
     }
+});
+
+router.get("/:id", verify_token, async (req, res) => {
+  try {
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+    const chat = await Chat.findOne({
+        participate: { $all: [senderId, receiverId] },
+    }).populate("messages");
+
+    if (!chat) {
+        return res.status(200).json([]);
+    }
+    const message = chat.messages;
+    res.status(200).json(message);
+
+  } catch (error) {
+    console.error("Error fetching messages: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 export default router;
